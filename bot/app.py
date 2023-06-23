9cffb9609739ccac0021baabb2337a1fc489f2ed
JR Cook <JCOOK10@hallmark.com>
2023-06-23 00:08:36 -0500

    trying to debug

--- bot/app.py
+++ bot/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from main import client
+import logging
 
 app = Flask(__name__)
+app.debug = True
+logging.basicConfig(level=logging.INFO)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+    logging.debug("index")
     return client.main()
 
 if __name__ == "__main__":
